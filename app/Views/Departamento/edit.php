ad5b7a3cbd0bb7ca90477a36966e5eb7f8c2e504
carmen siney <carmen.zet.usuario.2021@gmail.com>
2025-11-08 23:57:24 -0600

    cambie nombre

--- app/Views/Departamento/edit.php
+++ app/Views/Departamento/edit.php
@@ -34,9 +34,9 @@
 
             <!-- Botones de Acción -->
             <div class="d-flex justify-content-end mt-4">
-                <a href="<?= base_url('departamenta'); ?>" class="btn btn-secondary me-2">Cancelar</a>
+                <a href="<?= base_url('departamento'); ?>" class="btn btn-secondary me-2">Cancelar</a>
                 <button type="submit" class="btn btn-warning">
-                    <i class="fa-solid fa-sync-alt"></i> Actualizar Departamento
+                    <i class="fa-solid fa-sync-alt"></i> Departamento
                 </button>
             </div>
 
