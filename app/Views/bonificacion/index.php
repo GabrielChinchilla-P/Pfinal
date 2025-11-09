8f3acc0f8d2e0a50eb5a0a28a8e8faad0eb10c81
LMiran <lucia.miranda.201500997@gmail.com>
2025-11-09 01:10:52 -0600

    se modifico alertas

--- app/Views/bonificacion/index.php
+++ app/Views/bonificacion/index.php
@@ -10,6 +10,13 @@
         </button>
     </div>
 
+     <!-- ✅ Alertas -->
+    <?php if (session()->getFlashdata('success')): ?>
+        <div class="alert alert-success"><?= session()->getFlashdata('success') ?></div>
+    <?php elseif (session()->getFlashdata('error')): ?>
+        <div class="alert alert-danger"><?= session()->getFlashdata('error') ?></div>
+    <?php endif; ?>
+
 
 
 
