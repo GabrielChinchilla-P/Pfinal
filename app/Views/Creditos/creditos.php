09c5c48fef4a924472ecaad817a14f69dc008e86
GabrielChinchilla-P <gabriel.chinchilla.199919849.1@gmail.com>
2025-11-11 03:52:15 -0600

    Se modifico la vista de creditos

--- app/Views/Creditos/creditos.php
+++ app/Views/Creditos/creditos.php
@@ -21,18 +21,6 @@
                     </ul>
                 </td>
             </tr>
-        </tbody>
-    </table>
-
-    <table class="table table-bordered table-striped">
-        <thead class="table-dark text-center">
-            <tr>
-                <th>Carnet</th>
-                <th>Nombre</th>
-                <th>Procesos trabajados</th>
-            </tr>
-        </thead>
-        <tbody class="text-center">
             <tr>
                 <td>199919849</td>
                 <td>Gabriel Eduardo Chinchilla Pérez</td>
@@ -42,6 +30,39 @@
                         <li>Diseño e integración con Bootstrap 5 y DataTables</li>
                         <li>Implementación de modales y alertas dinámicas</li>
                         <li>Creación de Login para ingreso de usuarios y registro de nuevos usuarios</li>
+                    </ul>
+                </td>
+            </tr>
+            <tr>
+                <td>201500997</td>
+                <td>Ana Lucía Miranda De León</td>
+                <td class="text-start">
+                    <ul class="mb-0">
+                        <li>Desarrollo del CRUD de Bonificación (buscar, guardar, editar, eliminar)</li>
+                        <li>Diseño e integración con Bootstrap 5 y DataTables</li>
+                        <li>Implementación de modales y alertas dinámicas</li>
+                    </ul>
+                </td>
+            </tr>
+            <tr>
+                <td>202206632</td>
+                <td>José Carlos Gálvez Manzo</td>
+                <td class="text-start">
+                    <ul class="mb-0">
+                        <li>Desarrollo del CRUD de Informe de gastos (buscar, guardar, editar, eliminar)</li>
+                        <li>Diseño e integración con Bootstrap 5 y DataTables</li>
+                        <li>Implementación de modales y alertas dinámicas</li>
+                    </ul>
+                </td>
+            </tr>
+            <tr>
+                <td>202108186</td>
+                <td>Carmen Alicia Zet Siney</td>
+                <td class="text-start">
+                    <ul class="mb-0">
+                        <li>Desarrollo del CRUD de Departamentos (buscar, guardar, editar, eliminar)</li>
+                        <li>Diseño e integración con Bootstrap 5 y DataTables</li>
+                        <li>Implementación de modales y alertas dinámicas</li>
                     </ul>
                 </td>
             </tr>
