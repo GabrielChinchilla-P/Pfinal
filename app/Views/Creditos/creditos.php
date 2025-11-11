51145aa402c26bd84e299199c35608dfd78b67dd
GabrielChinchilla-P <gabriel.chinchilla.199919849.1@gmail.com>
2025-11-11 02:10:39 -0600

    Se agrego página de creditos

---
+++ app/Views/Creditos/creditos.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Créditos</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <header>
+        <h1>Créditos</h1>
+    </header>
+
+    <main>
+        <table class="table table-bordered table-striped">
+            <thead class="table-dark text-center">
+                <tr>
+                    <th>Carnet</th>
+                    <th>Nombre</th>
+                    <th>Procesos trabajados</th>
+                </tr>
+            </thead>
+            <tbody class="text-center">
+                <tr>
+                    <td>202112444</td>
+                    <td>Katherine Villavicencio</td>
+                    <td class="text-start">
+                        <ul class="mb-0">
+                            <li>Desarrollo del CRUD de Empleados (buscar, guardar, editar, eliminar)</li>
+                            <li>Diseño e integración con Bootstrap 5 y DataTables</li>
+                            <li>Implementación de modales y alertas dinámicas</li>
+                        </ul>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+
+        <table class="table table-bordered table-striped">
+            <thead class="table-dark text-center">
+                <tr>
+                    <th>Carnet</th>
+                    <th>Nombre</th>
+                    <th>Procesos trabajados</th>
+                </tr>
+            <tbody class="text-center">
+                <tr>
+                    <td>199919849</td>
+                    <td>Gabriel Eduardo Chinchilla Pérez</td>
+                    <td class="text-start">
+                        <ul class="mb-0">
+                            <li>Desarrollo del CRUD de Nómina (buscar, guardar, editar, eliminar)</li>
+                            <li>Diseño e integración con Bootstrap 5 y DataTables</li>
+                            <li>Implementación de modales y alertas dinámicas</li>
+                            <li>Creación de Login para ingreso de usuarios y registro de nuevos usuarios</li>
+                        </ul>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+
+        <div class="imagenes">
+            <img src="images/usac.png" alt="Logo USAC" width="150">
+            <img src="images/efpem.png" alt="Logo EFPEM" width="150">
+
+        </div>
+    </main>
+    <br><br><br>
+    <footer>
+        <p>© 2025 - Proyecto Final | Proyectos de Programación</p>
+    </footer>
+</body>
+
+</html>
